5502a173655ee842d4db36ba46f476461ea8ef49
TomLeggettDev <tomleggettdev@gmail.com>
2023-09-12 23:52:28 +1200

    added support for multi-page app on gh-pages
    taken from https://github.com/rafgraph/spa-github-pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import './App.css';
 import Header from './Header/Header.js';
-import Home from './Home/Home.js';
-import Watch from './Watch/Watch.js';
-import Merch from './Merch/Merch.js';
-import Shows from './Shows/Shows.js';
-import About from './About/About.js';
-import Contact from './Contact/Contact.js';
 import HomePage from './HomePage.js';
 import SecretPage from './SecretPage/SecretPage.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
